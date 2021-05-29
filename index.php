b1103efe335630aa4f8f21e7ebc37b02773f8123
Zeljko Djuranovic <zeljkobar@gmail.com>
2021-05-30 01:05:58 +0200

    starter

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Games</title>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+</head>
+
+<body>
+
+    <h1>prvi php sajt</h1>
+
+</body>
+
+</html>
