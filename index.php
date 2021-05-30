9a8f2b8cff3d80bf30c58d79967317a946f2e0a8
Zeljko Djuranovic <zeljkobar@gmail.com>
2021-05-30 03:19:44 +0200

    header

--- index.php
+++ index.php
@@ -6,11 +6,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Games</title>
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
 
-    <h1>prvi php sajt</h1>
+    <!-- header -->
+
+<header>
+    <div class="topBar container">
+        <p>Folow us: 
+            <a href=""><i class="fab fa-pinterest"></i></a>
+            <a href=""><i class="fab fa-facebook"></i></a>
+            <a href=""><i class="fab fa-twitter"></i></a>
+            <a href=""><i class="fab fa-dribbble"></i></a>
+        </p>
+    </div>
+    <nav class="container">
+        <a href=" " class="logo">Gaming <span>Theme</span></a>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Games</a></li>
+            <li><a href="">Reviews</a></li>
+            <li><a href="">News</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+    </nav>
+        <div class="uvp container">
+            <h1>Game on!</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. <br> Ipsa repellendus, magni delectus at qui fugiat?</p>
+            <button>Read more</button>
+        </div>
+</header>
 
 </body>
 
